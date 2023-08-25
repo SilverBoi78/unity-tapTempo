6399a820a5332b479862f733fb683c1baf2fc660
SilverBoi78 <96032631+SilverBoi78@users.noreply.github.com>
2023-08-25 17:18:03 -0500

    added movement and jump

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //player jump when screen is tapped
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector3(0, 12f, 0);
+        }
+
+        //player move forward on its own
+        rb.velocity = new Vector3(2f, rb.velocity.y, 0);
+    }
+}
